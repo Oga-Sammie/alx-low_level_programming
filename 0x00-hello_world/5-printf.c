59fbf0832cc3aa7bf969133ddc5e91001139da65
Sammie <kofano25@yahoo.com>
2023-02-16 12:59:55 -0800

    C programming using prinf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that uses the printf function
+ * Return: 0 if the program is a success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
