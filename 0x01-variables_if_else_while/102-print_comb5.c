b7a553f534b3c78bbcde5adf881769d083f73967
Sammie <kofano25@yahoo.com>
2023-02-18 13:10:31 -0800

    All possible combees of 2 two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,24 +10,25 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = 0; x <= 98; x++)
+	for (x = 0; x < 100; x++)
 	{
-		for (y = x + 1; y <= 99; y++)
+		for (y = 0; y < 100; y++)
 		{
-			putchar((x / 10) + '0');
-			putchar((y % 10) + '0');
-			putchar(' ');
-			putchar((y / 10) + '0');
-			putchar((y % 10) + '0');
-
-			if (x == 98 && y == 99)
-				continue;
-
-			putchar(44);
-			putchar(32);
+			if (x < y)
+			{
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
