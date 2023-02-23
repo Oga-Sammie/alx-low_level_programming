6c1091b904a2b579c64b67d60535c9c546137a8d
Sammie <kofano25@yahoo.com>
2023-02-23 10:32:36 -0800

    A trangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_triangle - print a triangle
+* @size: the size of the triangle
+*/
+
+void print_triangle(int size)
+{
+	int q, r, s;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (q = 1; q < size; q++)
+		{
+			for (r = size - q; r > 1; r--)
+			{
+				_putchar(32);
+			}
+			for (s = 0; s <= q; s++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
