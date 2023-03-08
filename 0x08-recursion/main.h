879ac678b835a35959e099eb027c83afb169cfdb
Sammie <kofano25@yahoo.com>
2023-03-08 11:58:36 -0800

    Prints a string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int sqrt_test(int x, int g);
+int is_prime_number(int n);
+int is_divisible(int num, int div);
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
+int str_length(char *str);
+int check_palindrome(char *s, int length, int i);
+
+#endif /* MAIN_H */
